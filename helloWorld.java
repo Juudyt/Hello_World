0676f6f7cbdf34f6a42e7a140c846ef4527441fc
Juudyt <judit_daw@hotmail.com>
2015-10-08 16:55:45 +0200

    preg 6

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,7 @@
 public class helloWorld{
 
 	public static void main (String[] ar){
-		System.out.print("Hello World");
+		System.out.print("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
 	}
 
 }
