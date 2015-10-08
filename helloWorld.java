8d8064fbc90b82f4a8a5a551073e270047804b4c
Juudyt <judit_daw@hotmail.com>
2015-10-08 17:46:28 +0200

    n

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,7 @@
 public class helloWorld{
 
 	public static void main (String[] ar){
-		System.out.print("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
+		System.out.print("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT!! ");
 	}
 
 }
