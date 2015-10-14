615edb64e9dc0652285f95986dfa3ab1cb9d2e65
Juudyt <judit_daw@hotmail.com>
2015-10-14 16:37:13 +0200

    seneca

--- helloWorld.java
+++ helloWorld.java
@@ -1,11 +1,7 @@
 public class helloWorld{
 
 	public static void main (String[] ar){
-<<<<<<< HEAD
-		System.out.print("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT!! ");
-=======
-		System.out.print("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
->>>>>>> 0676f6f7cbdf34f6a42e7a140c846ef4527441fc
+		System.out.print("Hello World: To forgive all is as inhuman as to forgive none ('Seneca'). Fortunately we have GIT");
 	}
 
 }
